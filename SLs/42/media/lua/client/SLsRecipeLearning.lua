deac44f094d80885a15b4765f6b56023bb4e01b5
Felix <felix1.johans1@gmail.com>
2026-09-16 17:18:05 +0200

    feat: remove free_recepies
    Remove free_recepies because its un necsessery

--- SLs/42/media/lua/client/SLsRecipeLearning.lua
+++ SLs/42/media/lua/client/SLsRecipeLearning.lua
@@ -1,8 +1,3 @@
-local FREE_RECIPES = {
-    "SLs_MakeLongsword"
-}
-
-
 
 local BOOK_RECIPES = {
     "SLs_MakeLongsword"
@@ -31,7 +26,6 @@ local function whenPlayerCreated(playerIndex, player)
     if not player or not player.getKnownRecipes then return end
     local mode = getCurrentMode()
     if mode <= 1 then return end
-    learnList(player,FREE_RECIPES)
     if mode >= 3 then
     learnList(player, BOOK_RECIPES)
     end
